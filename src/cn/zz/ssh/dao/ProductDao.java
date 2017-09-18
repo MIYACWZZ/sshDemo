b5455d82808f7f7611fefcd9229c1b4a459525bf
MIYACWZZ <379845866@qq.com>
2017-09-18 20:08:27 +0800

    Initial commit

---
+++ src/cn/zz/ssh/dao/ProductDao.java
@@ -0,0 +1,18 @@
+package cn.zz.ssh.dao;
+
+import cn.zz.ssh.domain.Product;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class ProductDao extends HibernateDaoSupport {
+
+
+    /**
+     * DAO中保存商品的方法
+     * @param product
+     */
+    public void save(Product product){
+        System.out.println("DAO中的save执行...");
+        //调用集成的HibernateDaoSupport中的模板的save方法去持久化对象
+        getHibernateTemplate().save(product);
+    }
+}
